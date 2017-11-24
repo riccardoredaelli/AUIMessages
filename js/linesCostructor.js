e4a27192cc30b0309faba3e88d470fdf45d74c59
Andrea Pasquali <andreapasquali@MacBook-Pro-di-Andrea.local>
2017-11-24 11:26:46 +0100

    js

---
+++ js/linesCostructor.js
@@ -0,0 +1,6 @@
+function init(){
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    scene = document.querySelector('#scene');
+    scene.background = new THREE.Color( 0x66ffff );
+}
