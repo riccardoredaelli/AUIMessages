6b4e7d62bd85ad6d5a33e36503d810414180f069
Andrea Pasquali <andreapasquali@MacBook-Pro-di-Andrea.local>
2017-12-04 13:57:05 +0100

    funzionante tutto tranne collider cleared

--- js/linesCostructor.js
+++ js/linesCostructor.js
@@ -17,10 +17,7 @@ function addMultipleListeners(element,events,handler,useCapture,args){
 
 
 var sp= document.createElement('a-sphere');
-sp.setAttribute('dynamic-body', {
-  mass: 1.5,
-  linearDamping: 0.005
-});
+
 sp.setAttribute('radius', '0.5');
 sp.setAttribute('visible', false);
 sp.setAttribute('color', 'yellow');
@@ -30,107 +27,82 @@ spStop.setAttribute('radius', '1');
 spStop.setAttribute('visible', true);
 spStop.setAttribute('color', 'red');
 spStop.setAttribute('position', '-20 5 -15');
-spStop.setAttribute('class','collidable');
-spStop.setAttribute('collider-check');
 
 
 scene.appendChild(sp);
 scene.appendChild(spStop);
 var raycaster=document.querySelector('#rc');
-var mouse=Boolean;
-mouse=false;
 raycaster.setAttribute('objects','#curve');
 var curve=document.querySelector('#curve');
 
+function roadExit(){
+    console.log('uscito dal percorso');
+    spStop.setAttribute('visible', true);
+    sp.setAttribute('visible', false);
+    spStop.setAttribute('position', '-20 5 -15');
+    //curve.removeEventListener('raycaster-intersected', raycaster);
+    console.log('eseguito road exit prima del remove event listener');
+    curve.removeEventListener('raycaster-intersected-cleared', roadExit);
+    console.log('eseguito road exit con il remove event listener teoricamente non dovrei vederlo?');
+}
 
+function intersection(){
+    curve.addEventListener('raycaster-intersected', function raycaster(e) {
+        console.log('sono dentro al raycaster intersected');
+        sp.setAttribute('visible', true);
+        sp.setAttribute('position', e.detail.intersection.point);
+        sp.addEventListener('mouseup', function(){
+            console.log('sono dentro al rilascio del mouse');
+            spStop.setAttribute('position', sp.getDOMAttribute('position'));
+            spStop.setAttribute('visible',true);
+            sp.setAttribute('visible', false);
+            curve.removeEventListener('raycaster-intersected-cleared', roadExit);
+            curve.removeEventListener('raycaster-intersected', raycaster);
+        })
+    })
+    curve.addEventListener('raycaster-intersected-cleared', roadExit);
+    console.log('eseguito e tornato su funzione intersection');    
+    curve.removeEventListener('raycaster-intersected', raycaster);
+    console.log('eseguito e teoricamente rimosso il controllo dei raycaster');
+    return;
+}
 
-scene.addEventListener('mousedown', function(){
-    mouse=true;
-    console.log('mouse cliccato e var mouse= '+ mouse);
+spStop.addEventListener('mousedown', function(){
+    spStop.setAttribute('visible',false);
+    intersection();
 })
 
+/*function doit(){
+    spStop.addEventListener('mousedown',function(e){
+        spStop.setAttribute('visible', false);
+        console.log('intersection and click');
+        sp.setAttribute('visible', true);
+        spStop.setAttribute('visible', false);
+        sp.addEventListener('mouseup', function(){
+            console.log('dentro mouse up');
+            spStop.pause();
+            curve.pause();
+            restartListener();
+        })
+    })
+}
 
-scene.addEventListener('mouseup', function(){
-    mouse=false;
-    console.log('mouse rilasciato e var mouse= '+ mouse);        
-})
+
 
 curve.addEventListener('raycaster-intersected', e=>{
-    spStop.setAttribute('visible', false);
-    console.log('intersection');
-    sp.setAttribute('visible', true);
     sp.setAttribute('position', e.detail.intersection.point);
-
+    doit(e);
+    console.log('intersezione');
 })
 
+function restartListener(){
+    console.log('restart listener partito');
+    spStop.start();
+}
 
-/*
 
-curve.addEventListener('raycaster-intersected-cleared', function(){
-    spStop.setAttribute('visible', true);
-    console.log('visibile!');
-    spStop.setAttribute('position', sp.getDOMAttribute('position'));
-    console.log('intersection cleared');
-    sp.setAttribute('visible', false);
- 
-})
-
-curve.addEventListener('raycaster-intersected', function(){
-    intersected=true;
-    console.log('raggi intersecati e intersected= '+ intersected);
-})
-
-curve.addEventListener('raycaster-intersected', (e) =>{
-    intersected=true;
-    sp.setAttribute('visible', true);
-    sp.setAttribute('color', 'yellow');
-    curve.addEventListener('mouseenter', function(){
-console.log('MOUSEENTER----intersected= '+ intersected);                    
-            if(intersected==true){
-                sp.setAttribute('color', 'green');
-                sp.setAttribute('position', e.detail.intersection.point); 
-                curve.addEventListener('mouseup', function(){
-console.log('MOUSEUP----intersected= '+ intersected);                    
-                    if (intersected==true){
-                        sp.setAttribute('color','yellow');
-                        return;
-                    }
-                    else
-                        sp.setAttribute('color','red');
-                })
-            return;
-            }
-            else return;
-    })
-})
-
-curve.addEventListener('raycaster-intersected-cleared', function(){
-    intersected=false;
-    console.log('raggi non più intersecati e intersected= '+ intersected);
-})*/
-
+//spStop.setAttribute('position', sp.getDOMAttribute('position'));
 //curve.addEventListener('raycaster-intersected', (e) => console.log(e.detail.intersection.point));
-
-/*var intersects=raycaster.intersectEls(curve, true);
-var currentIntersected
-curve.addEventListener('raycaster-intersected',function(){
-   if ( intersects.length > 0 ) {
-       if ( currentIntersected !== undefined ) {
-           currentIntersected.material.linewidth = 1;
-		}
-       currentIntersected = intersects[ 0 ].object;
-       currentIntersected.material.linewidth = 5;
-       sp.visible = true;
-       sp.position.copy( intersects[ 0 ].point );
-        } else {
-            if ( currentIntersected !== undefined ) {
-                currentIntersected.material.linewidth = 1;
-            }
-            currentIntersected = undefined;
-            sp.visible = false;
-            }
-       // renderer.render( scene, camera );
-})*/
 
 function handler(e) {
     console.log('sono in handler');
@@ -143,18 +115,4 @@ addMultipleListeners(
     ['raycaster-intersected','mouseup'],
     handler,
     false);
-
-/*function fun(e){
-    console.log('esegue fun');
-    spStop.setAttribute('visible', true);
-    console.log('visibile!');
-    spStop.setAttribute('position', sp.getDOMAttribute('position'));
-    console.log('intersection cleared');
-    sp.setAttribute('visible', false);
-}
-
-addMultipleListeners(
-    curve,
-    ['raycaster-intersected','mouseup'],
-    fun,
-    false);*/
+*/
